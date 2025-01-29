09711b6d8509490997ccb741bbad364d250e061b
Kunwar Awadhiya <kunwarawadhiya@gmail.com>
2025-01-29 22:44:02 +0530

    Update About.jsx

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -2,8 +2,6 @@ import React,{useState, useEffect} from "react";
 import Navbar from "../Navbar/Navbar.jsx";
 import Hero from "../Hero/Hero.jsx";
 
-// import BgVideo from '../../assets/earth-bg.mp4';
-// import BgVideo from '../../assets/bailvideo.mp4'; 
 import BgVideo from '../../assets/yoga.mp4'; 
 import AOS from "aos";
 import "aos/dist/aos.css";
